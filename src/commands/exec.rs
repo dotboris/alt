a4cbdade1b8d4814af12ebc0f04970d3e70014a9
Boris Bera <bbera@pmctire.com>
2018-07-16 22:14:33 -0400

    Add loading definitions

--- src/commands/exec.rs
+++ src/commands/exec.rs
@@ -3,7 +3,9 @@ use std::env;
 
 pub fn exec(command: &str, command_args: Vec<&str>) {
     println!("TODO: exec {} {:?}", command, command_args);
-    let file = versions::find_file(env::current_dir().unwrap());
+    let command_def = versions::load_def_for(command);
+    println!("{:?}", command_def);
+    let file = versions::find_use_file(env::current_dir().unwrap());
     println!("{:?}", file);
     let bin = match file {
         Some(file) => "foobar",
--- src/versions.rs
+++ src/versions.rs
@@ -1,10 +1,17 @@
-use std::path::PathBuf;
+extern crate toml;
 
-const FILE_NAME: &'static str = ".alt.yml";
+use std::env;
+use std::path::{Path, PathBuf};
+use std::collections::HashMap;
+use std::fs;
 
-pub fn find_file(mut dir: PathBuf) -> Option<PathBuf> {
+const VERSIONS_FILE_NAME: &'static str = ".alt.toml";
+const DEFS_FILE_NAME: &'static str = "defs.toml";
+const DEFAULT_HOME: &'static str = ".config/alt";
+
+pub fn find_use_file(mut dir: PathBuf) -> Option<PathBuf> {
     loop {
-        let file = dir.join(FILE_NAME);
+        let file = dir.join(VERSIONS_FILE_NAME);
 
         if file.is_file() {
             return Some(file);
@@ -15,5 +22,35 @@ pub fn find_file(mut dir: PathBuf) -> Option<PathBuf> {
                 dir.pop();
             }
         }
+    }
+}
+
+fn home_dir() -> PathBuf {
+    match env::var("ALT_HOME") {
+        Ok(home) => PathBuf::from(home),
+        Err(_) => {
+            let home = env::var("HOME").unwrap();
+            Path::new(&home).join(DEFAULT_HOME)
+        },
+    }
+}
+
+pub type CommandVersions = HashMap<String, String>;
+pub type CommandDefs = HashMap<String, CommandVersions>;
+
+fn command_versions(raw_defs: String, command: &str) -> CommandVersions {
+    let all_defs: CommandDefs = toml::from_str(&raw_defs)
+        .expect("failed to parse definitions toml");
+
+    all_defs.get(command)
+        .map(|r| r.clone())
+        .unwrap_or_else(|| CommandVersions::new())
+}
+
+pub fn load_def_for(command: &str) -> CommandVersions {
+    let def_file_path = home_dir().join(DEFS_FILE_NAME);
+    match fs::read_to_string(def_file_path) {
+        Ok(contents) => command_versions(contents, command),
+        Err(_) => CommandVersions::new(),
     }
 }
