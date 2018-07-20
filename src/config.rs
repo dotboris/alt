e4bcfd5c105d163d6ce9db0fd0e7f1b31824635c
Boris Bera <bbera@pmctire.com>
2018-07-20 09:15:07 -0400

    Add tests for home_dir()

--- src/config.rs
+++ src/config.rs
@@ -12,3 +12,29 @@ pub fn home_dir() -> PathBuf {
         },
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::env;
+    use config;
+    use std::path::{Path, PathBuf};
+
+    #[test]
+    fn home_dir_should_default() {
+        env::remove_var("ALT_HOME");
+        assert_eq!(
+            config::home_dir(),
+            Path::new(&env::var("HOME").unwrap())
+                .join(config::DEFAULT_HOME)
+        )
+    }
+
+    #[test]
+    fn home_dir_should_read_alt_home_env() {
+        env::set_var("ALT_HOME", "/path/to/phony/home");
+        assert_eq!(
+            config::home_dir(),
+            PathBuf::from("/path/to/phony/home")
+        )
+    }
+}
