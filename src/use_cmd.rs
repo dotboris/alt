3ff1648490cc2b7eee56f0ad00f59c969cca4bb9
Boris Bera <bera.boris@gmail.com>
2018-07-28 11:46:29 -0400

    support special case for `system` as version arg

--- src/use_cmd.rs
+++ src/use_cmd.rs
@@ -18,27 +18,43 @@ pub fn run(command: &str, arg_version: Option<&str>) {
 
     let version = arg_version
         .unwrap_or_else(|| prompt_version(command_versions));
-    let bin = command_versions.get(version)
-        .unwrap_or_else(|| {
-            println!("Unknown version {} for command {}", version, command);
-            println!("Did you forget to define it? (see alt help scan)");
-            process::exit(2);
-        });
 
-    let cwd = env::current_dir().unwrap();
-    let use_file = use_file::find_or_dir(&cwd);
-    let mut use_def = use_file::load(&use_file).unwrap_or_default();
-    use_def.insert(String::from(command), String::from(version));
-    use_file::save(&use_def, &use_file)
-        .expect(&format!("Failed to write use file to {}", use_file.to_str().unwrap()));
+    if version == "system" {
+        let cwd = env::current_dir().unwrap();
+        let use_file = use_file::find_or_dir(&cwd);
+        let mut use_def = use_file::load(&use_file).unwrap_or_default();
+        use_def.remove(command);
+        use_file::save(&use_def, &use_file)
+            .expect(&format!("Failed to write use file to {}", use_file.to_str().unwrap()));
 
-    println!(
-        "Will now use {} {} ({}) when in {}",
-        command,
-        version,
-        bin.to_str().unwrap(),
-        use_file.parent().unwrap().to_str().unwrap()
-    );
+        println!(
+            "Will now use system version of {} when in {}",
+            command,
+            use_file.parent().unwrap().to_str().unwrap()
+        );
+    } else {
+        let bin = command_versions.get(version)
+            .unwrap_or_else(|| {
+                println!("Unknown version {} for command {}", version, command);
+                println!("Did you forget to define it? (see alt help scan)");
+                process::exit(2);
+            });
+
+        let cwd = env::current_dir().unwrap();
+        let use_file = use_file::find_or_dir(&cwd);
+        let mut use_def = use_file::load(&use_file).unwrap_or_default();
+        use_def.insert(String::from(command), String::from(version));
+        use_file::save(&use_def, &use_file)
+            .expect(&format!("Failed to write use file to {}", use_file.to_str().unwrap()));
+
+        println!(
+            "Will now use {} {} ({}) when in {}",
+            command,
+            version,
+            bin.to_str().unwrap(),
+            use_file.parent().unwrap().to_str().unwrap()
+        );
+    }
 }
 
 fn prompt_version(versions: &def_file::CommandVersions) -> &str {
