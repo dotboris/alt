dccb589d5556aaa94bcf518e039d16b5178080be
Boris Bera <bera.boris@gmail.com>
2018-07-29 13:45:03 -0400

    Extract command helpers to TestEnv impl

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,60 +1,42 @@
 extern crate assert_cmd;
 
-use std::process::Command;
 use assert_cmd::prelude::*;
 use std::env;
-use std::path::*;
-use assert_cmd::cargo;
 mod test_env;
+use test_env::TestEnv;
 
-fn def_command(env: &Path, alt_bin: &Path, command: &str, version: &str, path: &Path) -> Command {
-    let mut c = Command::new(alt_bin);
-    test_env::set_env(&mut c, env);
-    c.args(&["def", command, version, path.to_str().unwrap()]);
-    c
-}
-
-fn use_command(env: &Path, alt_bin: &Path, command: &str, version: &str) -> Command {
-    let mut c = Command::new(alt_bin);
-    test_env::set_env(&mut c, env);
-    c.args(&["use", command, version]);
-    c
-}
-
-fn command(env: &Path, name: &str) -> Command {
-    let mut c = Command::new(name);
-    test_env::set_env(&mut c, env);
-    c
+fn def_all(env: &TestEnv) {
+    for command in &["alfa", "bravo", "charlie"] {
+        for version in &[1, 2, 3] {
+            env.def(
+                command, &version.to_string(),
+                &test_env::fixture_command_path(command, version.clone())
+            )
+                .assert()
+                .success();
+        }
+    }
 }
 
 #[test]
 fn def_and_use() {
-    let alt_bin = cargo::main_binary_path().unwrap();
-    let env = test_env::create_env();
+    let env = TestEnv::new();
 
-    def_command(
-        &env, &alt_bin,
-        "alfa", "1",
-        &test_env::fixture_command_path("alfa", 1)
-    )
+    def_all(&env);
+
+    env::set_current_dir(&env.root).unwrap();
+
+    env.command("alfa")
+        .assert()
+        .success()
+        .stdout("alfa system\n");
+
+    env._use("alfa", "1")
         .assert()
         .success();
 
-    env::set_current_dir(&env).unwrap();
-
-    command(&env, "alfa")
-        .assert()
-        .failure()
-        .stdout("command not found: alfa\n");
-
-    use_command(&env, &alt_bin, "alfa", "1")
-        .assert()
-        .success();
-
-    command(&env, "alfa")
+    env.command("alfa")
         .assert()
         .success()
         .stdout("alfa1\n");
-
-    test_env::delete_env(env);
 }
--- tests/test_env.rs
+++ tests/test_env.rs
@@ -3,35 +3,76 @@ extern crate assert_cmd;
 
 use self::rand::prelude::*;
 use self::rand::distributions::Alphanumeric;
+use std::ffi::OsStr;
 use std::env;
 use std::fs;
 use std::path::*;
 use std::process::Command;
-use assert_cmd::prelude::*;
+use assert_cmd::cargo;
 
-pub fn create_env() -> PathBuf {
-    let rand_ns: String = thread_rng()
-        .sample_iter(&Alphanumeric)
-        .take(10)
-        .collect();
-
-    let dir = env::temp_dir()
-        .join(format!("alt-tests-{}", rand_ns));
-
-    fs::create_dir(&dir)
-        .expect(&format!("failed to created tmp env {:?}", &dir));
-    dir
+pub struct TestEnv {
+    pub root: PathBuf,
+    project_root: PathBuf,
+    alt_bin: PathBuf,
 }
 
-pub fn delete_env(dir: PathBuf) {
-    fs::remove_dir_all(&dir)
-        .expect(&format!("failred to remove {:?}", &dir));
+impl TestEnv {
+    pub fn new() -> Self {
+        let rand_ns: String = thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(10)
+            .collect();
+
+        let root = env::temp_dir()
+            .join(format!("alt-tests-{}", rand_ns));
+
+        fs::create_dir(&root)
+            .expect(&format!("failed to created tmp env {:?}", &root));
+
+        TestEnv {
+            root: root,
+            project_root: env::current_dir().unwrap(),
+            alt_bin: cargo::main_binary_path().unwrap(),
+        }
+    }
+
+    pub fn command<T: AsRef<OsStr>>(&self, name: T) -> Command {
+        let mut c = Command::new(name);
+        set_env(&mut c, self);
+        c
+    }
+
+    pub fn alt(&self) -> Command {
+        self.command(&self.alt_bin)
+    }
+
+    pub fn def(&self, command: &str, version: &str, path: &Path) -> Command {
+        let mut c = self.alt();
+        c.args(&["def", command, version, path.to_str().unwrap()]);
+        c
+    }
+
+    pub fn _use(&self, command: &str, version: &str) -> Command {
+        let mut c = self.alt();
+        c.args(&["use", command, version]);
+        c
+    }
 }
 
-pub fn set_env<'a>(c: &'a mut Command, test_env: &Path) -> &'a mut Command {
-    c.env("ALT_HOME", test_env.join("alt-home"));
-    c.env("ALT_SHIM_DIR", test_env.join("shims"));
-    c.env("PATH", test_env.join("shims"))
+impl Drop for TestEnv {
+    fn drop(&mut self) {
+        fs::remove_dir_all(&self.root)
+            .expect(&format!("failred to remove {:?}", &self.root));
+    }
+}
+
+fn set_env<'a>(c: &'a mut Command, test_env: &TestEnv) -> &'a mut Command {
+    c.env("ALT_HOME", test_env.root.join("alt-home"));
+    c.env("ALT_SHIM_DIR", test_env.root.join("shims"));
+    c.env("PATH", env::join_paths(&[
+        test_env.root.join("shims"),
+        test_env.project_root.join("fixtures/system_commands")
+    ]).unwrap())
 }
 
 pub fn fixture_command_path(name: &str, version: i32) -> PathBuf {
