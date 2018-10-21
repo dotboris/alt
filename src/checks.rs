a683a59aff498b05e16f1366ad1a57eb4b339a4c
Boris Bera <bbera@pmctire.com>
2018-10-21 14:03:31 -0400

    Make warning a big easy to see box

--- src/checks.rs
+++ src/checks.rs
@@ -1,6 +1,29 @@
 use config;
 use std::env;
+use std::iter;
+use console;
 use console::style;
+
+fn line(width: usize) -> String {
+    let line: String = iter::repeat("=").take(width).collect();
+    return format!("{}", style(line).bold());
+}
+
+fn line_label(width: usize, label: &str) -> String {
+    let label_size = label.len() + 2;
+    let left_size = (width - label_size) / 2;
+    let right_size = width - left_size - label_size;
+
+    let left: String = iter::repeat(">").take(left_size).collect();
+    let right: String = iter::repeat("<").take(right_size).collect();
+
+    return format!(
+        "{} {} {}",
+        style(left).bold(),
+        style(label).bold().red(),
+        style(right).bold()
+    );
+}
 
 pub fn check_shim_in_path() {
     let path = env::var("PATH").unwrap();
@@ -8,14 +31,29 @@ pub fn check_shim_in_path() {
 
     let shim_dir = config::shim_dir();
     if !parts.any(|part| part == shim_dir) {
+        let term = console::Term::stdout();
+        let (_, term_witdh) = term.size();
+
+        let shim_dir = shim_dir.to_str().unwrap();
+        let shim_dir = style(shim_dir).cyan();
+        let path_env_var = style("PATH").cyan();
+
+        eprintln!("{}", line_label(term_witdh as usize, "WARNING"));
+        eprintln!("Alt is not installed corrected and will not work!");
+        eprintln!();
         eprintln!(
-            "{}: Shims are not in your {}. Alt will not function properly.",
-            style("WARNING").bold().yellow(),
-            style("PATH").bold().cyan()
+            "You are seeing the warning because the shim directory ({}) is not in your {} environment variable.",
+            shim_dir,
+            path_env_var
         );
+        eprintln!();
+        eprintln!("Please add {} to your {} environment variable.", shim_dir, path_env_var);
+        eprintln!();
         eprintln!(
-            "See {} for setup instructions.",
+            "Alternatively, see {} for setup instructions.",
             style("https://github.com/dotboris/alt#installation").cyan()
         );
+        eprintln!("{}", line(term_witdh as usize));
+        eprintln!();
     }
 }
