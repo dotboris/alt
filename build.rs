541975993ef9794e460adfc51af07f5d869efd2b
Boris Bera <bera.boris@gmail.com>
2019-01-04 20:58:59 -0500

    Require the out_dir

--- build.rs
+++ build.rs
@@ -6,10 +6,7 @@ use std::env;
 include!("src/cli.rs");
 
 pub fn main() {
-    let outdir = match env::var_os("OUT_DIR") {
-        None => return,
-        Some(outdir) => outdir,
-    };
+    let outdir = env::var_os("OUT_DIR").unwrap();
     let mut app = make_app();
     app.gen_completions("alt", Shell::Bash, &outdir);
     app.gen_completions("alt", Shell::Zsh, &outdir);
