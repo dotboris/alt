b2eb30f2296600878fb46d572227e2f0b6c7574c
Boris Bera <bera.boris@gmail.com>
2019-08-25 13:53:19 -0400

    Rebuild package script to support steps

--- ci/package.py
+++ ci/package.py
@@ -1,42 +1,76 @@
 #!/usr/bin/env python3
 import subprocess
-from os import path, mkdir
-from shutil import copy, move
 from argparse import ArgumentParser
+from os import path, makedirs
+from shutil import copy, move, rmtree
+from tempfile import mkdtemp
+
+
+def step(message):
+    print("\033[1m>>> {0}\033[0m".format(message))
 
 
 def sh(*args):
+    return subprocess.run(args, check=True)
+
+
+def sh_capture(*args):
     return subprocess.check_output(args)
+
+
+def build_release(rust_target):
+    step('Building release build for target {}'.format(rust_target))
+    sh('cargo', 'build', '--release', '--target', rust_target)
+
+
+def build_gzip_bin(bin_path, version, rust_target, dest_dir):
+    step('Packinging {0} as a gzipped binary'.format(bin_path))
+    work_dir = mkdtemp()
+
+    to_gzip_file = path.join(work_dir, 'alt')
+    copy(bin_path, to_gzip_file)
+    sh('gzip', '-fk9', to_gzip_file)
+    move(
+        '{}.gz'.format(to_gzip_file),
+        path.join(dest_dir, 'alt_v{0}_{1}.gz'.format(version, rust_target))
+    )
+
+    rmtree(work_dir)
+
+
+def list_output(dest_dir):
+    step('Contents of {}'.format(dest_dir))
+    sh('ls', '-lh', dest_dir)
 
 
 def parse():
     parser = ArgumentParser()
-    parser.add_argument('--alt-bin', required=True)
     parser.add_argument('--dest-dir', required=True)
     parser.add_argument('--rust-target', required=True)
     return parser.parse_args()
 
 
 def main(
-    alt_bin=None,
     dest_dir=None,
     rust_target=None,
 ):
-    mkdir(dest_dir)
-    dest_alt_bin = path.join(dest_dir, 'alt')
-    copy(alt_bin, dest_alt_bin)
+    step('Emptying {}'.format(dest_dir))
+    if (path.exists(dest_dir)):
+        rmtree(dest_dir)
+    makedirs(dest_dir)
 
-    version = sh(dest_alt_bin, '--version')
-    version = str(version)
+    build_release(rust_target)
+    alt_bin = path.join('target', rust_target, 'release/alt')
+
+    step('Looking up version')
+    version = sh_capture(alt_bin, '--version')
+    version = version.decode()
     version = version.strip().split(' ')[1]
+    print(version)
 
-    gz_dir = path.join(dest_dir, 'gz-bin')
-    mkdir(gz_dir)
-    sh('gzip', '-fk9', dest_alt_bin)
-    move(
-        '{0}.gz'.format(dest_alt_bin),
-        path.join(gz_dir, 'alt_v{0}_{1}.gz'.format(version, rust_target))
-    )
+    build_gzip_bin(alt_bin, version, rust_target, dest_dir)
+
+    list_output(dest_dir)
 
 
 if __name__ == "__main__":
