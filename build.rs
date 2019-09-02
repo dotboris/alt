b388bcfc5e38c0f9ea01e9a72c16c2f747a11e74
Boris Bera <bera.boris@gmail.com>
2019-09-02 08:58:09 -0400

    Output completion in easy to find dir

--- build.rs
+++ build.rs
@@ -2,13 +2,23 @@
 extern crate clap;
 use clap::Shell;
 use std::env;
+use std::path::PathBuf;
+use std::fs;
 
 include!("src/cli.rs");
 
 pub fn main() {
-    let outdir = env::var_os("OUT_DIR").unwrap();
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let completion_dir = PathBuf::from(&out_dir);
+    let mut completion_dir = completion_dir
+        .ancestors()  // .../target/<debug|release>/build/example-<SHA>/out
+        .skip(3)      // .../target/<debug|release>
+        .next().unwrap().to_owned();
+    completion_dir.push("completion");
+
+    fs::create_dir_all(&completion_dir).unwrap();
     let mut app = make_app();
-    app.gen_completions("alt", Shell::Bash, &outdir);
-    app.gen_completions("alt", Shell::Zsh, &outdir);
-    app.gen_completions("alt", Shell::Fish, &outdir);
+    app.gen_completions("alt", Shell::Bash, &completion_dir);
+    app.gen_completions("alt", Shell::Zsh, &completion_dir);
+    app.gen_completions("alt", Shell::Fish, &completion_dir);
 }
