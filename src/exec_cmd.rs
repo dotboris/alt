0f8d77404c91273eca821715cf79642521e55138
Boris Bera <bbera@pmctire.com>
2020-11-20 17:11:27 -0500

    Add verbose debugging output when alt fails to exec something

--- src/exec_cmd.rs
+++ src/exec_cmd.rs
@@ -3,12 +3,14 @@ use crate::command;
 use std::os::unix::process::CommandExt;
 use std::process;
 use std::process::Command;
+use std::env;
 
 pub fn run(command: &str, command_args: &[String]) {
     let defs = def_file::load();
     let command_version = command::find_selected_version(&command);
 
     let bin = command_version
+        .clone()
         .map(|version| {
             def_file::find_bin(&defs, command, &version)
                 .unwrap_or_else(|| panic!(
@@ -26,11 +28,20 @@ pub fn run(command: &str, command_args: &[String]) {
                 .args(command_args)
                 .exec();
 
-            // Since we're callling exec, either our process will be replaced
+            // Since we're calling exec, either our process will be replaced
             // (and this code will never be called) or something's wrong and
             // we get this error
-            eprintln!("Failed to exec()!");
-            eprintln!("{:#?}", err);
+            eprintln!("🔥 alt failed to run {}!", command);
+            eprintln!("error: {:?}", err);
+            eprintln!("command: {}", command);
+            eprintln!("command version: {}",
+                command_version
+                    .as_deref()
+                    .unwrap_or("(using system version)")
+            );
+            eprintln!("args: {:?}", command_args);
+            eprintln!("bin: {}", bin.display());
+            eprintln!("current dir: {:?}", env::current_dir());
             panic!();
         },
         None => {
