3e6354d8728f665b17ac5691fbdab6e034051a11
Boris Bera <bbera@pmctire.com>
2020-11-21 01:41:05 -0500

    Extract function to check defs

--- src/doctor_cmd.rs
+++ src/doctor_cmd.rs
@@ -1,4 +1,5 @@
 use crate::def_file;
+use crate::def_file::CommandDefs;
 use std::collections::HashSet;
 use std::path::Path;
 use std::process;
@@ -13,67 +14,25 @@ pub fn run(fix: bool) {
     );
     println!();
 
-    let mut problem_count = 0;
-    let mut fixable_count = 0;
-    let mut fixed_count = 0;
+    let mut problem_count: u32 = 0;
+    let mut fixable_count: u32 = 0;
+    let mut fixed_count: u32 = 0;
 
-    let defs = def_file::load();
-
-    if defs.is_empty() {
-        problem_count += 1;
-        print_problem(
-            "No commands or command versions are defined. This is normal if \
-            you've just installed alt. You will need to define some commands \
-            & command versions in order to use alt. See: \
-            https://github.com/dotboris/alt#define-command-versions"
-        );
-        println!();
-    }
-
-    let mut defs_to_remove = HashSet::new();
-    for (command, versions) in defs {
-        for (version, bin) in versions {
-            let has_problem = def_has_problem(&command, &version, &bin);
-            if has_problem {
-                problem_count += 1;
-
-                defs_to_remove.insert(
-                    (command.to_string(), version.to_string())
-                );
-
-                if fix {
-                    fixed_count += 1;
-                    print_fixed(&format!(
-                        "Removed entry for {} version {}.",
-                        command, version
-                    ));
-                } else {
-                    fixable_count += 1;
-                    print_fixable(&format!(
-                        "Would remove entry for {} version {}.",
-                        command, version
-                    ));
-                }
-                println!();
-            }
-        }
-    }
+    let CheckDefsRes { defs, removed_defs } = check_defs(
+        fix,
+        &mut problem_count,
+        &mut fixable_count,
+        &mut fixed_count
+    );
 
     if fix {
-        let mut defs = def_file::load();
-        for (command, version) in defs_to_remove {
-            let versions = defs.get_mut(&command).unwrap();
-            versions.remove(&version);
-            if versions.is_empty() {
-                defs.remove(&command);
-            }
-        }
         def_file::save(&defs)
-            .expect("failed to write command versions definition");
+            .expect("failed to write command version definitions");
     }
 
     // TODO: check all used versions point to real versions
     // TODO: check that shims are defined
+    // TODO: check that old shims are not left over
 
     if problem_count > 0 {
         if fix {
@@ -98,8 +57,7 @@ pub fn run(fix: bool) {
             }
         }
 
-        let problems_left = 0.max(problem_count - fixed_count);
-        if problems_left > 1 {
+        if problem_count > fixed_count {
             process::exit(1);
         }
     } else {
@@ -110,6 +68,75 @@ pub fn run(fix: bool) {
         );
     }
 }
+
+struct CheckDefsRes {
+    defs: CommandDefs,
+    removed_defs: HashSet<(String, String)>
+}
+fn check_defs(
+    fix: bool,
+    problem_count: &mut u32,
+    fixable_count: &mut u32,
+    fixed_count: &mut u32
+) -> CheckDefsRes {
+    let defs = def_file::load();
+
+    if defs.is_empty() {
+        *problem_count += 1;
+        print_problem(
+            "No commands or command versions are defined. This is normal if \
+            you've just installed alt. You will need to define some commands \
+            & command versions in order to use alt. See: \
+            https://github.com/dotboris/alt#define-command-versions"
+        );
+        println!();
+    }
+
+    let mut defs_to_remove = HashSet::new();
+
+    for (command, versions) in defs {
+        for (version, bin) in versions {
+            let has_problem = def_has_problem(&command, &version, &bin);
+            if has_problem {
+                *problem_count += 1;
+
+                defs_to_remove.insert(
+                    (command.to_string(), version.to_string())
+                );
+
+                if fix {
+                    *fixed_count += 1;
+                    print_fixed(&format!(
+                        "Removed entry for {} version {}.",
+                        command, version
+                    ));
+                } else {
+                    *fixable_count += 1;
+                    print_fixable(&format!(
+                        "Would remove entry for {} version {}.",
+                        command, version
+                    ));
+                }
+                println!();
+            }
+        }
+    }
+
+    let mut defs = def_file::load();
+    for (command, version) in defs_to_remove.clone() {
+        let versions = defs.get_mut(&command).unwrap();
+        versions.remove(&version);
+        if versions.is_empty() {
+            defs.remove(&command);
+        }
+    }
+
+    return CheckDefsRes {
+        defs,
+        removed_defs: defs_to_remove
+    }
+}
+
 
 fn def_has_problem(command: &str, version: &str, bin: &Path) -> bool {
     if !bin.exists() {
