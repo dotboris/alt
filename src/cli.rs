be3097e5652af3f3c89de66b9b3a4e82d67ab7f2
Boris Bera <bera.boris@gmail.com>
2020-11-28 10:19:23 -0500

    Migrate away from clap macro and use builder pattern
    Using the clap macro was a mistake. It's a whole other macro to use with
    pretty poor documentation. This is now much easier to use and
    understand. If you need a new feature, you can just call the right
    method and not try to guess how to use that with the macro.
    
    It also allows us to use long flags with `-` in the middle of them

--- src/cli.rs
+++ src/cli.rs
@@ -1,73 +1,105 @@
-use clap::App;
-use clap::AppSettings;
+use clap::{ App, AppSettings, SubCommand, Arg };
 
 pub fn make_app() -> App<'static, 'static> {
-    clap_app!(alt =>
-        (version: crate_version!())
-        (about: "Switch between different versions of commands")
-        (settings: &[
+    return App::new("alt")
+        .version(crate_version!())
+        .about("Switch between different versions of commands")
+        .settings(&[
             AppSettings::SubcommandRequiredElseHelp,
             AppSettings::VersionlessSubcommands
         ])
-        (@subcommand exec =>
-            (about: "Run the given command")
-            (after_help:
+
+        .subcommand(SubCommand::with_name("exec")
+            .about("Run the given command")
+            .after_help(
 "ARGS NOTE:
-    Note that `alt exec` handles some flags on its own (`--help` for example).
-    If you want to pass such arguments to the executed command instead of
-    `alt exec`, you will need to use `--` to tell alt exec to stop parsing
-    arguments.
+Note that `alt exec` handles some flags on its own (`--help` for example).
+If you want to pass such arguments to the executed command instead of
+`alt exec`, you will need to use `--` to tell alt exec to stop parsing
+arguments.
 
-    Example:
+Example:
 
-    alt exec node --help        # --help passed to alt (shows this message)
-    alt exec node -- --help     # --help passed to node (shows node's help)"
+alt exec node --help        # --help passed to alt (shows this message)
+alt exec node -- --help     # --help passed to node (shows node's help)"
             )
-            (@arg command: +required "The command to run")
-            (@arg command_args: ... "Arguments to pass to the command")
+            .arg(Arg::with_name("command")
+                .help("The command to run")
+                .required(true)
+            )
+            .arg(Arg::with_name("command_args")
+                .help("Arguments to pass to the command")
+                .multiple(true),
+            )
         )
-        (@subcommand shim =>
-            (about: "Generate shims for all managed commands")
+
+        .subcommand(SubCommand::with_name("shim")
+            .about("Generate shims for all managed commands")
         )
-        (@subcommand which =>
-            (about: "Print the resolved path of a command")
-            (@arg command: +required "Command to look up")
+
+        .subcommand(SubCommand::with_name("which")
+            .about("Print the resolved path of a command")
+            .arg(Arg::with_name("command")
+                .required(true)
+                .help("Command to look up")
+            )
         )
-        (@subcommand scan =>
-            (about: "Scan for different versions of the given command")
-            (@arg command: +required "Command to scan for")
+
+        .subcommand(SubCommand::with_name("scan")
+            .about("Scan for different versions of the given command")
+            .arg(Arg::with_name("command")
+                .required(true)
+                .help("Command to scan for")
+            )
         )
-        (@subcommand use =>
-            (about: "Swtich the version of a command")
-            (after_help:
+
+        .subcommand(SubCommand::with_name("use")
+            .about("Swtich the version of a command")
+            .after_help(
 "EXAMPLES:
     alt use node 8        Use version 8 of node
     alt use node          Prompt for a version of node to use
     alt use node system   Use the system version of node"
             )
-            (@arg command: +required "Command to switch the version of")
-            (@arg version: "Version to use (optional)")
-        )
-        (@subcommand show =>
-            (about: "Print commands and their versions")
-        )
-        (@subcommand doctor =>
-            (about: "Checks if alt is setup correctly. Helps debug problems.")
-            (@arg fix_mode:
-                // TODO: use a dash instead of an underscore
-                --fix_mode -f
-                +required
-                +takes_value
-                possible_value[auto never prompt]
-                default_value[prompt]
-                "Control how automatic fixes are applied."
+            .arg(Arg::with_name("command")
+                .required(true)
+                .help("Command to switch the version of")
+            )
+            .arg(Arg::with_name("version")
+                .help("Version to use (optional)")
             )
         )
-        (@subcommand def =>
-            (about: "Define a new version")
-            (@arg command: +required "Command to define the version for")
-            (@arg version: +required "The name of the version")
-            (@arg bin: +required "Path to the executable for the version")
+
+        .subcommand(SubCommand::with_name("show")
+            .about("Print commands and their versions")
         )
-    )
+
+        .subcommand(SubCommand::with_name("def")
+            .about("Define a new version")
+            .arg(Arg::with_name("command")
+                .required(true)
+                .help("Command to define the version for")
+            )
+            .arg(Arg::with_name("version")
+                .required(true)
+                .help("The name of the version")
+            )
+            .arg(Arg::with_name("bin")
+                .required(true)
+                .help("Path to the executable for the version")
+            )
+        )
+
+        .subcommand(SubCommand::with_name("doctor")
+            .about("Checks if alt is setup correctly. Helps debug problems.")
+            .arg(Arg::with_name("fix_mode")
+                .short("f")
+                .long("fix-mode")
+                .takes_value(true)
+                .possible_values(&["auto", "never", "prompt"])
+                .default_value("prompt")
+                .help("Control how automatic fixes are applied.")
+            )
+        )
+        ;
 }
