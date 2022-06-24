936ece4673bfdaddc0ae116e266f58ed397e5b2c
Boris Bera <bera.boris@gmail.com>
2022-06-24 11:27:39 -0400

    Generate new changelog by parsing sections
    The previous implementation would look for specific markers and make
    chnages there. This new implementation allows us to extract sections of
    the changelog.

--- ci/add_changelog_version.py
+++ ci/add_changelog_version.py
@@ -1,12 +1,37 @@
 #!/usr/bin/env python3
 from argparse import ArgumentParser
 from datetime import date
+from enum import Enum, auto
 from pathlib import Path
+from pprint import pprint
+import re
 
 REPO_URL = "https://github.com/dotboris/alt"
 
 
-def parse():
+class Section(Enum):
+    PREAMBLE = "preamble"
+    UNRELEASED_BODY = "unreleased-body"
+    PREVIOUS_RELEASES = "previous-releases"
+    REFS = "refs"
+
+
+def parse_changelog(changelog_path: Path) -> dict[Section, list[str]]:
+    sections = {location: [] for location in Section}
+
+    location = Section.PREAMBLE
+    with changelog_path.open("r") as fh:
+        for line in fh.readlines():
+            if match := re.match(r"<!-- section:(?P<section>.*) -->", line):
+                section = match.group("section")
+                location = Section(section)
+
+            sections[location].append(line)
+
+    return sections
+
+
+def parse_args():
     parser = ArgumentParser()
     parser.add_argument("--old-version", required=True)
     parser.add_argument("--new-version", required=True)
@@ -17,24 +42,24 @@ def main(*, old_version, new_version):
     root = Path(__file__).parent.parent
     changelog_path = root / "CHANGELOG.md"
 
-    new_changelog = []
-    with changelog_path.open("r") as fh:
-        for line in fh.readlines():
-            new_changelog.append(line)
+    sections = parse_changelog(changelog_path)
 
-            if "<!-- release:new-version-section -->" in line:
-                today = date.today()
-                new_changelog.append(f"\n## [{new_version}] {today.isoformat()}\n")
-            elif "<!-- release:new-version-ref -->" in line:
-                new_changelog.append(
-                    f"[{new_version}]: "
-                    f"{REPO_URL}/compare/{old_version}..{new_version}\n"
-                )
+    today = date.today()
+    new_changelog = [
+        *sections[Section.PREAMBLE],
+        "<!-- section:unreleased-body -->\n",
+        f"\n## [{new_version}] {today.isoformat()}\n",
+        *sections[Section.UNRELEASED_BODY][1:],
+        *sections[Section.PREVIOUS_RELEASES],
+        "<!-- section:refs -->\n",
+        f"[{new_version}]: {REPO_URL}/compare/{old_version}..{new_version}\n",
+        *sections[Section.REFS][1:],
+    ]
 
     with changelog_path.open("w") as fh:
         fh.writelines(new_changelog)
 
 
 if __name__ == "__main__":
-    args = parse()
+    args = parse_args()
     main(**vars(args))
