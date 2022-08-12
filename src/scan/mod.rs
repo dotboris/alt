a3a57c88b5ecacd84bba784fe5d784a4a398afc3
Boris Bera <bera.boris@gmail.com>
2022-08-12 17:47:08 -0400

    Autofix clippy stuff

--- src/scan/mod.rs
+++ src/scan/mod.rs
@@ -3,7 +3,7 @@ use std::path::*;
 pub mod homebrew;
 pub mod path_suffix;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct CommandVersion {
     pub command: String,
     pub version: String,
