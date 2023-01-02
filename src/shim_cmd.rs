9c8884f524de21f661f6d63d7d243c6fffb8a8f5
Boris Bera <bera.boris@gmail.com>
2023-01-02 16:18:45 -0500

    Remove funny invisible char from shim command output

--- src/shim_cmd.rs
+++ src/shim_cmd.rs
@@ -12,7 +12,7 @@ pub fn run() -> anyhow::Result<()> {
     for command in command_version_registry.command_names() {
         let res = shim::make_shim(&command, &env::current_exe().unwrap());
         match res {
-            Ok(()) => println!(" {}️ {}", style("✓").green().bold(), command),
+            Ok(()) => println!(" {} {}", style("✓").green().bold(), command),
             Err(err) => println!(" {} {}: {}", style("✗").red().bold(), command, err),
         }
     }
