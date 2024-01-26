db036267d996844a40acbee3d20cf4222e840b0b
Boris Bera <beraboris@gmail.com>
2024-01-26 15:09:05 -0500

    Fixup flake imports

--- flake.nix
+++ flake.nix
@@ -1,40 +1,46 @@
 {
-  description =
-    "Switches between different versions of commands based on your current directory";
+  description = "Switches between different versions of commands based on your current directory";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
-    rust-overlay.url = "github:oxalica/rust-overlay/stable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay/stable";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        # Overlays enable you to customize the Nixpkgs attribute set
-        overlays = [
-          # Makes a `rust-bin` attribute available in Nixpkgs
-          (import rust-overlay)
-          # Provides a `rustToolchain` attribute for Nixpkgs that we can use to
-          # create a Rust environment
-          (self: super: {
-            rustToolchain = super.rust-bin.stable.latest.default.override {
-              targets = pkgs.lib.optionals pkgs.stdenv.isLinux [
-                # We package using musl on linux
-                "x86_64-unknown-linux-musl"
-              ];
-            };
-          })
-        ];
-        pkgs = import nixpkgs { inherit system overlays; };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      # Overlays enable you to customize the Nixpkgs attribute set
+      overlays = [
+        # Makes a `rust-bin` attribute available in Nixpkgs
+        (import rust-overlay)
+        # Provides a `rustToolchain` attribute for Nixpkgs that we can use to
+        # create a Rust environment
+        (self: super: {
+          rustToolchain = super.rust-bin.stable.latest.default.override {
+            targets = pkgs.lib.optionals pkgs.stdenv.isLinux [
+              # We package using musl on linux
+              "x86_64-unknown-linux-musl"
+            ];
+          };
+        })
+      ];
+      pkgs = import nixpkgs {inherit system overlays;};
+    in {
+      formatter = pkgs.alejandra;
 
-      in
-      {
-        formatter = pkgs.nixpkgs-fmt;
-
-        devShells.default = pkgs.mkShell {
-          # The Nix packages provided in the environment
-          packages = (with pkgs; [
+      devShells.default = pkgs.mkShell {
+        # The Nix packages provided in the environment
+        packages =
+          (with pkgs; [
             # The usual suite for rust tools including cargo, Clippy,
             # cargo-fmt rustdoc, rustfmt, and other tools.
             rustToolchain
@@ -54,7 +60,8 @@
 
             shellcheck
             cargo-insta
-          ]) ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [ pkgs.libiconv ];
-        };
-      });
+          ])
+          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [pkgs.libiconv];
+      };
+    });
 }
